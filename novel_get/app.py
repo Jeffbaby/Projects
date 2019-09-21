9a2f619a34648b3c7d50980a5043a5a9364e91a5
Jeffbaby <mjfeng110@163.com>
2019-09-21 22:54:38 +0800

    清除空格

--- novel_get/app.py
+++ novel_get/app.py
@@ -6,13 +6,15 @@ def get_one_chapter(url):
     #url = "http://www.biquge.se/24901/41015693.html"
     response = requests.get(url, headers=headers)
     res_xpath = etree.HTML(response.text)
-    title = res_xpath.xpath('//h1/text()')[0]
+    title1 = res_xpath.xpath('//h1/text()')[0]
+    title = title1.strip()  #去掉文本前后空格
     content = res_xpath.xpath("//div[@id='content']/text()")
     with open(file="绝世战神.txt", mode='a+', encoding='UTF-8') as f:
-        title = str.strip(title)
-        f.write(title)
         f.write("\n\n")
+        f.write(title)
+        f.write("\n")
         for line in content:
+            line = line.strip()  #去掉文本前后空格
             f.write(line)
         print(title,"下载完成")
 
@@ -20,10 +22,12 @@ def get_one_chapter(url):
 response = requests.get('http://www.biquge.se/24901/')
 res_xpath = etree.HTML(response.text)
 urls = res_xpath.xpath("//*[@id='list']/dl/dd/a/@href")
-
 for i,url in enumerate(urls):
     if i>8:
         get_one_chapter("http://www.biquge.se"+url)
         print("http://www.biquge.se"+url)
     else:
         pass
+
+
+
